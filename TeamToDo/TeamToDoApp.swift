818e0473af60496136c13dc0863abef8fd3d9994
asasakk <asasakkfml@gmail.com>
2025-12-16 15:45:29 +0900

    Initial Commit

---
+++ TeamToDo/TeamToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamToDoApp.swift
+//  TeamToDo
+//
+//  Created by asai on 2025/12/16.
+//
+
+import SwiftUI
+
+@main
+struct TeamToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
